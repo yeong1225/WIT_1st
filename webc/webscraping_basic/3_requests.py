f5351b856aaa1d361d7a6993bbc221e353ae2da5
yeong1225 <99941578+yeong1225@users.noreply.github.com>
2023-05-22 01:15:17 +0900

    Delete webc directory

--- webc/webscraping_basic/3_requests.py
+++
@@ -1,8 +0,0 @@
-import requests
-res = requests.get("http://google.com")
-res.raise_for_status() #문제 생기면 오류 뱉고 출력 x
-print(len(res.text))
-print(res.text)
-
-with open("mygoogle.html","w",encoding ="utf8") as f:
-    f.write(res.text)
--- webc/webscraping_basic/5_user_agents.py
+++
@@ -1,7 +0,0 @@
-import requests
-url = "http://nadocoding.tistory.com"
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"}
-res = requests.get(url, headers=headers)
-res.raise_for_status() #문제 생기면 오류 뱉고 출력 x
-with open("nadocoding.html","w",encoding ="utf8") as f:
-    f.write(res.text)
